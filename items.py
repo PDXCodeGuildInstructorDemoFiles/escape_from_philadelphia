baec5279a710fb86fd3895350223e0f6e40a8b4e
Chase Daniels <titanspeed@gmail.com>
2017-04-12 20:17:48 -0700

    added Items class

---
+++ items.py
@@ -0,0 +1,24 @@
+class Items:
+    def __init__(self, name, type, effect, weight):
+        self.name = name
+        self.type = type
+        self.effect = effect
+        self.weight = weight
+
+    def weapon(self, name, type, effect, weight)
+        self.name = name
+        self.type = type
+        self.effect = effect
+        self.weight = weight
+
+    def armor(self, name, type, effect, weight):
+        self.name = name
+        self.type = type
+        self.effect = effect
+        self.weight = weight
+
+    def first_aid(self, name, type, effect, weight):
+        self.name = name
+        self.type = type
+        self.effect = effect
+        self.weight = weight
