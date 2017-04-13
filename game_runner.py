0c9e486586cc50e607c558b0533f28ff6ce36f8c
Christopher Jones <chris.charles.jones@gmail.com>
2017-04-12 20:29:46 -0700

    showing random generation.

---
+++ game_runner.py
@@ -0,0 +1,21 @@
+from map import Map
+
+
+class Game:
+    def __init__(self):
+        self.map = Map()
+
+    def create_monsters(self):
+        pass
+
+    def create_player(self):
+        pass
+        name = input("What is your name adventurer?: ")
+
+    def start_game(self):
+        pass
+
+    def interface(self):
+        while True:
+            pass
+game = Game()
--- items.py
+++ items.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Items:
     def __init__(self, name, type, effect, weight):
         self.name = name
@@ -5,20 +8,28 @@ class Items:
         self.effect = effect
         self.weight = weight
 
-    def weapon(self, name, type, effect, weight)
-        self.name = name
-        self.type = type
-        self.effect = effect
-        self.weight = weight
 
-    def armor(self, name, type, effect, weight):
-        self.name = name
-        self.type = type
-        self.effect = effect
-        self.weight = weight
+def weapon(effect):
+    names = ['sword of might', 'wooden sword']
+    return Items(random.choice(names), 'weapon', effect, random.randrange(5, 10))
 
-    def first_aid(self, name, type, effect, weight):
-        self.name = name
-        self.type = type
-        self.effect = effect
-        self.weight = weight
+
+def armor(self, name, type, effect, weight):
+    self.name = name
+    self.type = type
+    self.effect = effect
+    self.weight = weight
+
+
+def first_aid(self, name, type, effect, weight):
+    self.name = name
+    self.type = type
+    self.effect = effect
+    self.weight = weight
+
+
+sword = weapon(-20)
+
+print(sword.name)
+print(sword.weight)
+
