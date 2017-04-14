1ada5c78627d148df9a0a89ee7f994f6bffd9851
Tab Dansby <tab.titus@gmail.com>
2017-04-13 21:20:02 -0700

    Added to weapons, first aid, code comments

--- items.py
+++ items.py
@@ -13,23 +13,32 @@ class Items:
 
 
 def weapon(effect):
+    """This function generates a random weapon with random weight"""
     w_names = ['sword of might', 'wooden sword', 'big axe', 'dagger']
     return Items(random.choice(w_names), 'weapon', effect, random.randrange(5, 10))
 
 
 def armor():
-    a_names = ('steel chest plate', 'iron helmet', 'leather gloves', 'spiked helm')
+    """This function generates a random piece of armor with random effect (defense) and weight"""
+    a_names = ('steel chest plate', 'iron helmet', 'leather gloves', 'spiked helmet')
     return Items(random.choice(a_names), 'armor', random.randrange(1, 50), random.randrange(5, 20))
 
-def first_aid(self, name, type, effect, weight):
-    self.name = name
-    self.type = type
-    self.effect = effect
-    self.weight = weight
+def first_aid():
+    """This function generates random first aid items of random effect (healing). All weigh one pound"""
+    fa_names = ['cake', 'cheesesteak', 'water', 'potion', 'coffee', "soft pretzels"]
+    return Items(random.choice(fa_names), 'first aid', random.randrange(1, 40), 1)
 
-helmet = armor()
-sword = weapon(-20)
 
-print(sword)
-print(helmet)
+
+#Character item selection (possibly add this to inventory.py...?):
+#First aid item would add 1 pound from inventory when picked up, subtract 1 pound when used, add to health when used
+#Weapon would add variable pounds when picked up, subtract those pounds when dropped, subtract from opponent's health
+#Armor adds variable pounds when picked up, subtracts pounds when dropped, decreases impact (divide
+
+#helmet = armor(10)
+#sword = weapon(-20)
+
+print(armor())
+print(first_aid())
+#print(helmet)
 
