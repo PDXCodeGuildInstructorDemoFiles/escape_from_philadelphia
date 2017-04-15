868aa33c1a29c71d336e345bea38c8700005e72c
Tab Dansby <tab.titus@gmail.com>
2017-04-14 21:36:27 -0700

    Added to inventory, sketched out functions/methods for adding/dropping items in inventory

--- inventory.py
+++ inventory.py
@@ -33,13 +33,27 @@ class Inventory:                                                                
     def add_item(self, item):                                                                                                     #picking up/adding an item.
         # up_item = input("You come across a {} on the ground. Do you want to pick it up? \(Y\)es or \(N\)o?: ").format(self.local_item).upper()
         # if up_item == "Y":
+              if bag_slots +1 > 10:
+                  print("Bag is full!")
+
+              elif bag_weight < 100:
+                  print("Bag will be too heavy! Can't add this!")
+              else:
+                  #add-to-inventory function here
+
+
         #     player_inv.append(self.local_item)
-        #     print("You pick up the {}.").format(self.local_item)                                                      #all for shit
+        #     print("You picked up the {}.").format(self.local_item)                                                      #all for shit
         # elif up_item == "N":
         #     print("You leave the {} where it lies.").format(self.local_item)
-         self.inventory[1] = item                #no earthly idea what this means                                       #may need to create method for trade/purchase
+         self.inventory[+1] = item                #no earthly idea what this means (why does the second item in the inventory need a variable?)    #may need to create method for trade/purchase
 
 def use_item(self):#more bullshit to follow
+    use_response = input("Do you want to V(iew) your")
+
+    print player_inv
+    player_inv[position]
+
     pass
 
 
@@ -49,6 +63,8 @@ def enable_item(self):
 
 def list_item(self):
     pass
+
+
 
 
 weapon = Items("sword", "weapon", 20, 5)                                                                                #hard coded shit.
