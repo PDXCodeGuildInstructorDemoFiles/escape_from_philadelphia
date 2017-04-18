3e650b6a32af51345b89626a1a07561b69f17be8
Christopher Jones <chris.charles.jones@gmail.com>
2017-04-17 20:06:01 -0700

    merging all files to master

--- inventory.py
+++ inventory.py
@@ -1,34 +1,34 @@
 class Inventory:
 
-    def __init__(self, position, name, type, weight):
-        self.position = position
+    def __init__(self, name, weight):
         self.name = name
-        self.type = type
         self.weight = weight
         self.coin = 0
+        self.inv = self.create_inv()
+
+    def create_inv(self, slots=15):
+        dictionary = {}
+        for i in range(1, slots + 1):
+            dictionary[i] = 'empty'
+        return dictionary
 
     def __str__(self):
-        return 'Position  Name    Type    Weight  \n     {}    {}    {}     {}   '.format(self.position, self.name, self.type, self.weight)
-        return str(char_inv)
+        return self.inv
+        # return 'Position  Name    Type    Weight  \n     {}    {}    {}     {}   '.format(self.position, self.name, self.type, self.weight)
     # def __repr__(self):
     #     return str(char_inv)
 
     # def use_item(self):
 
+inventory = Inventory('player bags', 50)
+print(inventory.inv)
 
 
-
-
-
-
-
-
-
-char_inv = []
-knife = Inventory(1,"knife","wep",5)
-gun = Inventory(2, "gun", "wep", 1)
-
-char_inv.append(gun)
-char_inv.append(knife)
-for x in char_inv:
-    print(x)
+# char_inv = []
+# knife = Inventory(1,"knife","wep",5)
+# gun = Inventory(2, "gun", "wep", 1)
+#
+# char_inv.append(gun)
+# char_inv.append(knife)
+# for x in char_inv:
+#     print(x)
